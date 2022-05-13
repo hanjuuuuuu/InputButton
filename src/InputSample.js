504ab5ef5e916ba179ac9f7c0dca32a667034fde
hanjuuuuuu <hanjucomss@naver.com>
2022-05-13 12:48:18 +0900

    first commit

---
+++ src/InputSample.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function InputSample() {
+    return (
+        <div>
+            <input />
+            <button>버튼</button>
+            
+        </div>
+    );
+}
+
+export default InputSample;
